db3f612ca75b124b2487817b97cb0d467c19b7ea
UncodeGama <unculkodegama@yahoo.com>
2021-11-24 22:31:59 +0100

    added more tests

--- anton/tests/test_weatherapp_endpoints.py
+++ anton/tests/test_weatherapp_endpoints.py
@@ -11,24 +11,38 @@ class TestWeatherAppEndpoints:
     def setup_class(cls):
         create_db_file()
 
-    def test_get_locations_status(self):
+    def test_get_locations(self):
         response = self.client.get("weather/locations")
         assert response.status_code == 200
-
-    def test_get_locations_data(self):
-        response = self.client.get("weather/locations")
         assert len(response.json()) == 4
         assert response.json()[0]['location'] == 'Brno'
         assert response.json()[0]['location'] != 'brno'
         assert response.json()[0]['lat'] == '49.1922443'
         assert response.json()[0]['lon'] == '16.6113382'
 
-    def test_get_weather_forecast_by_location_status(self):
+    def test_get_weather_forecasts(self):
+        response = self.client.get("weather/forecasts")
+        assert response.status_code == 200
+        assert len(response.json()) == 4
+
+    def test_get_weather_forecast_by_location(self):
         response = self.client.get("weather/forecasts/Brno")
         assert response.status_code == 200
-
-    def test_get_weather_forecast_by_location_data(self):
-        response = self.client.get("weather/forecasts/Brno")
         assert len(response.json()) == 6
         assert response.json()['location'] == 'Brno'
         assert response.json()['data']['lat'] == '49.1922'
+        assert response.json()['data']['lon'] == '16.6113'
+
+    def test_get_weather_forecast_by_location_current(self):
+        response = self.client.get("weather/forecasts/Brno/current")
+        assert response.status_code == 200
+
+    def test_get_weather_forecast_by_location_hourly(self):
+        response = self.client.get("weather/forecasts/Brno/hourly")
+        assert response.status_code == 200
+
+    def test_get_weather_forecast_by_location_daily(self):
+        response = self.client.get("weather/forecasts/Brno/daily")
+        assert response.status_code == 200
+
+
