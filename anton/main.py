450c258296a1534cf1c8976555b64cd12e492d9b
UncodeGama <unculkodegama@yahoo.com>
2021-11-28 22:11:40 +0100

    added pyinstaller

--- anton/main.py
+++ anton/main.py
@@ -7,20 +7,14 @@ from anton.utils.database import create_db_file
 from anton.utils.static import UPDATE_TIME
 from anton.weatherapp.endpoints.endpoints import weather
 
-
-def app():
-    app = FastAPI(title="Anton v2.0", description="Backend for Anton application.")
-
-    app.include_router(weather)
-
-    return app
-
+app = FastAPI(title="Anton v2.0", description="Backend for Anton application.")
+app.include_router(weather)
 
 if __name__ == "__main__":
     create_db_file()
     timer_run(UPDATE_TIME, core.call_open_weather_api_forecasts)
 
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, log_level="info")
+    uvicorn.run(app, host="127.0.0.1", port=8000, log_level="info")
 
 # python -m uvicorn main:app --reload
 
