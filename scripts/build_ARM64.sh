7c7958263346777ad4b31cb495eace5c22cc9001
UncodeGama <unculkodegama@yahoo.com>
2021-12-02 19:40:21 +0100

    added workflow for arm64

--- scripts/build_ARM64.sh
+++ scripts/build_ARM64.sh
@@ -3,4 +3,4 @@
 set -e
 set -x
 
-pyinstaller --onefile --icon=static/64x64.ico --clean --name Anton --console --upx-dir /home/runner/work/Anton/Anton/upx-3.96-arm64_linux --key=$1  ./anton/main.py
+pyinstaller --onefile --clean --name Anton --console --key=$1  ./anton/main.py
